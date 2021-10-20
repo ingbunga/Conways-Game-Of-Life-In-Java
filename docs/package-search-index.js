1a7d5b833d628192442f524ade883a8f4b148756
ingbunga <20sunrin129@sunrint.hs.kr>
2021-10-20 14:26:04 +0900

    docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.sungu.console"},{"l":"com.sungu.game_of_life"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.sungu.console"},{"l":"com.sungu.game_of_life"},{"l":"com.sungu.game_of_life.rbt"}];updateSearchResults();
