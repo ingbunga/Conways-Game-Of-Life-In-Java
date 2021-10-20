af65357b66065e9064f826ecb01960b7be0a3999
ingbunga <20sunrin129@sunrint.hs.kr>
2021-10-20 09:48:05 +0900

    docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.sungu.console"},{"l":"com.sungu.game_of_life"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.sungu.console"},{"l":"com.sungu.game_of_life"}];updateSearchResults();
--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.sungu.game_of_life","l":"Cell"},{"p":"com.sungu.game_of_life","l":"Grid"},{"p":"com.sungu.console","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.sungu.game_of_life","l":"Board2D"},{"p":"com.sungu.game_of_life","l":"Board3D"},{"p":"com.sungu.game_of_life","l":"Cell"},{"p":"com.sungu.console","l":"Test2D"},{"p":"com.sungu.console","l":"Test3D"}];updateSearchResults();
