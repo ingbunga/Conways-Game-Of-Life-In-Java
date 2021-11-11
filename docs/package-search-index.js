819004d69cba2564eec91583f4a3456d987d6d86
ingbunga <20sunrin129@sunrint.hs.kr>
2021-11-11 13:55:24 +0900

    remove height, width from rbt tree

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.sungu.game_of_life"},{"l":"com.sungu.game_of_life.rbt"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.sungu.console"},{"l":"com.sungu.game_of_life"},{"l":"com.sungu.game_of_life.rbt"}];updateSearchResults();
